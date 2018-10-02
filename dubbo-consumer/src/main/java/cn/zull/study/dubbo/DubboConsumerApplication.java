c97a3b02f9b5903d9b583b081df3cccd888c30fb
ZuRun's MacBook Pro <hi@zuhr.me>
2018-10-02 23:17:39 +0800

    链路日志收集handler自定义

--- dubbo-consumer/src/main/java/cn/zull/study/dubbo/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/cn/zull/study/dubbo/DubboConsumerApplication.java
@@ -1,7 +1,8 @@
-package cn.zull.study.dubbo.consumer;
+package cn.zull.study.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author zurun
